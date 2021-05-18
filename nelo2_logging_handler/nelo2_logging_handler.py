901e3b0dae9c2c14164e62f6ae5e7c5f049cd03f
ultimatelife <>
2021-05-18 13:22:13 +0900

    sync logging handler added

--- nelo2_logging_handler/nelo2_logging_handler.py
+++ nelo2_logging_handler/nelo2_logging_handler.py
@@ -1,9 +1,12 @@
 import asyncio
 import logging
 import socket
+from asyncio import ensure_future
 from threading import Thread
+from typing import Optional
 
-import httpx
+import aiohttp
+import requests
 
 
 class Nelo2Exception(Exception):
@@ -13,6 +16,34 @@ class Nelo2Exception(Exception):
 
 class Nelo2LoggingHandler(logging.Handler):
     def __init__(self, project_name: str, project_version: str, end_point: str,
+                 host: str = None, timeout: int = None, level: int = logging.NOTSET, default_header: dict = None):
+        super().__init__(level)
+        self.project_name = project_name
+        self.project_version = project_version
+        self.host = host or socket.gethostname()
+        self.end_point = end_point
+        self.default_header = default_header
+        self.timeout = timeout or 4
+        self.session: requests.Session = requests.Session()
+
+    def emit(self, record: logging.LogRecord) -> None:
+        body = {
+            'projectName': self.project_name,
+            'projectVersion': self.project_version,
+            'body': record.msg,
+            'host': self.host,
+            'logLevel': record.levelname
+        }
+
+        res = self.session.post(self.end_point, json=body, timeout=self.timeout, headers=self.default_header)
+        try:
+            res.raise_for_status()
+        except:
+            raise Nelo2Exception(res.text)
+
+
+class AsyncNelo2LoggingHandler(logging.Handler):
+    def __init__(self, project_name: str, project_version: str, end_point: str,
                  host: str = None, loop: asyncio.AbstractEventLoop = None,
                  timeout: int = None, level: int = logging.NOTSET, default_header: dict = None):
         super().__init__(level)
@@ -21,17 +52,11 @@ class Nelo2LoggingHandler(logging.Handler):
         self.host = host or socket.gethostname()
         self.end_point = end_point
         self.default_header = default_header
-        self._client: httpx.AsyncClient = None
         self.timeout = timeout or 10
+        self._loop: Optional[asyncio.AbstractEventLoop] = loop
+        self._session: Optional[aiohttp.ClientSession] = None
 
-        if loop is not None:
-            self.loop = loop
-        else:
-            self.loop = asyncio.new_event_loop()
-            t = Thread(target=self.start_background_loop, args=(self.loop,), daemon=True)
-            t.start()
-
-    def start_background_loop(self, loop: asyncio.AbstractEventLoop):
+    def start_daemon_loop(self, loop: asyncio.AbstractEventLoop):
         print("run start_background_loop start")
         asyncio.set_event_loop(loop)
         loop.run_forever()
@@ -39,34 +64,44 @@ class Nelo2LoggingHandler(logging.Handler):
         print("run start_background_loop done")
 
     @property
-    def client(self) -> httpx.AsyncClient:
-        if self._client is None or self._client.is_closed:
-            print("new client is created")
-            self._client = httpx.AsyncClient()
-            return self._client
-        return self._client
+    def session(self) -> aiohttp.ClientSession:
+        if self._session is None or self._session.closed:
+            connector = aiohttp.TCPConnector(family=socket.AF_INET,
+                                             loop=self.loop)
+            self._session = aiohttp.ClientSession(connector=connector,
+                                                  loop=self.loop)
+        return self._session
 
-    def emit(self, record):
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        if self._loop is None or self._loop.is_closed():
+            self._loop = asyncio.new_event_loop()
+            t = Thread(target=self.start_daemon_loop, args=(self._loop,), daemon=True)
+            t.start()
+        return self._loop
+
+    def emit(self, record: logging.LogRecord):
         async def e():
             try:
-                payload = {
+                body = {
                     'projectName': self.project_name,
                     'projectVersion': self.project_version,
+                    'body': f'{record.msg} {len(asyncio.all_tasks(self.loop))}',
                     'host': self.host,
-                    'body': record.msg,
-                    'logSource': record.name,
                     'logLevel': record.levelname
                 }
 
-                await asyncio.sleep(10)
-                res: httpx.Response = await self.client.post(url=self.end_point, timeout=self.timeout, json=payload, )
-                if res.status_code != 200:
-                    raise Nelo2Exception(res)
-            except (KeyboardInterrupt, SystemExit) as e:
-                print(e)
+                async with self.session.post(self.end_point, json=body, headers=self.default_header,
+                                             timeout=self.timeout) as resp:
+                    if resp.status != 200:
+                        raise Nelo2Exception()
+                    try:
+                        resp.raise_for_status()
+                    except:
+                        raise Nelo2Exception(resp)
+            except (KeyboardInterrupt, SystemExit):
                 raise
             except:
-                # raise
                 self.handleError(record)
 
-        asyncio.ensure_future(e(), loop=self.loop)
+        ensure_future(e(), loop=self.loop)
--- tests/test_nelo2_logging_handler.py
+++ tests/test_nelo2_logging_handler.py
@@ -1,16 +1,35 @@
 import logging
 import os
 from datetime import datetime
+from time import sleep
 
 from nelo2_logging_handler import __version__
-from nelo2_logging_handler.nelo2_logging_handler import Nelo2LoggingHandler
+from nelo2_logging_handler.nelo2_logging_handler import AsyncNelo2LoggingHandler, Nelo2LoggingHandler
 
 
 def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_nelo2_handler():
+def test_async_nelo2_handler():
+    root_logger = logging.getLogger("test_logger")
+    root_logger.setLevel("DEBUG")
+    root_formatter = logging.Formatter("%(asctime)s - %(name)s:%(lineno)d %(funcName)20s - %(levelname)s - %(message)s")
+
+    nelo2_handler = AsyncNelo2LoggingHandler(project_name=os.environ['PROJECT_NAME'], project_version='0.1',
+                                             end_point=os.environ['END_POINT'], )
+    nelo2_handler.setFormatter(root_formatter)
+    root_logger.addHandler(nelo2_handler)
+    # HTTPHandler
+    logger = logging.getLogger("test_logger")
+
+    for i in range(10):
+        logger.info(f"nelo2 handler info: {datetime.now()}")
+        logger.debug(f"nelo2 handler info: {datetime.now()}")
+        sleep(2)
+
+
+def test_sync_nelo2_handler():
     root_logger = logging.getLogger("test_logger")
     root_logger.setLevel("DEBUG")
     root_formatter = logging.Formatter("%(asctime)s - %(name)s:%(lineno)d %(funcName)20s - %(levelname)s - %(message)s")
@@ -22,6 +41,7 @@ def test_nelo2_handler():
     # HTTPHandler
     logger = logging.getLogger("test_logger")
 
-    for i in range(10):
+    for i in range(100):
         logger.info(f"nelo2 handler info: {datetime.now()}")
-        logger.debug(f"nelo2 handler debug: {datetime.now()}")
+        logger.debug(f"nelo2 handler info: {datetime.now()}")
+        sleep(0.1)
